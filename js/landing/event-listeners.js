632c8943d01a3b633a73caa2a3cac88d3e1db87d
Jayden-747 <jaydenhutchinson747@gmail.com>
2024-04-15 09:51:55 -0700

    update to path structure and added landing page

---
+++ js/landing/event-listeners.js
@@ -0,0 +1,13 @@
+/**
+ * Adds on click to login button
+ */
+$("#login").click(function () {
+  console.log("hi");
+});
+
+/**
+ * Adds on click to sign-up button
+ */
+$("#sign-up").click(function () {
+  console.log("hi");
+});
