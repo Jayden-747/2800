3f4f8440bf80132c0ac44139cf8dd9c505bc2b10
Tommy Phuong  <tphuong4@my.bcit.ca>
2024-05-15 10:16:46 -0700

    Changed home authentication that routes to the home page or landing page based on a valid session

--- index.js
+++ index.js
@@ -76,12 +76,13 @@ app.use("/plantepedia", express.static("./views/plantepedia"));
 
 // LANDING PAGE
 app.get("/", (req, res) => {
-  res.render("landing/landing");
+  if(req.session.authenticated){
+    res.render("home/home");
+  } else {
+    res.render("landing/landing");
+  }
 });
 
-app.get("/home", (req, res) => {
-  res.render("home/home");
-});
 
 // SIGNUP PAGE
 app.get("/signup", async (req, res) => {
@@ -124,9 +125,6 @@ app.post("/signup/submitUser", async (req, res) => {
   req.session.cookie.maxAge = expireTime;
 
   res.redirect("/");
-
-
-
 })
 
 
@@ -141,17 +139,13 @@ app.post("/login/logging", async (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
 
-  const schema = Joi.string().max(20).required();
+  const schema = Joi.string().max(255).required();
 	const validationResult = schema.validate(email);
 	if (validationResult.error != null) {
 	   console.log(validationResult.error);
 	   res.redirect("/login");
 	   return;
 	}
-
-
-
-
   const result = await userCollection.find({email: email}).project({email: 1, password: 1, _id: 1, username: 1}).toArray();
 
   if (result.length != 1) {
@@ -165,6 +159,7 @@ app.post("/login/logging", async (req, res) => {
     req.session.email = email;
     req.session.cookie.maxAge = expireTime;
     res.redirect("/");
+    console.log("logged in");
     return;
   } else {
     res.redirect("/login");
