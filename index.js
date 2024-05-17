9d8d08b490fb809b9e5b3721278ad171704fc81c
Yujin Jeong <te0209494@gmail.com>
2024-05-16 21:16:48 -0700

    Co-authored-by: neriyel <neriyel@users.noreply.github.com>

--- index.js
+++ index.js
@@ -31,13 +31,12 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 
 //mongodb database
 var { database } = require("./databaseConnection");
-
 const userCollection = database.db(mongodb_database).collection("users");
 const gardensCollection = database.db(mongodb_database).collection("gardens");
 
-const plantSummaryCollection = database
+const plantCollection = database
   .db(mongodb_database_plantepedia)
-  .collection("plantSummary");
+  .collection("plant");
 const plantDetailsCollection = database
   .db(mongodb_database_plantepedia)
   .collection("plantDetails");
@@ -58,16 +57,6 @@ const saltRounds = 12;
 //Expiration for cookie
 const expireTime = 1 * 60 * 60 * 1000;
 
-//session
-app.use(
-  session({
-    secret: node_session_secret,
-    store: mongoStore,
-    saveUninitialized: false,
-    resave: true,
-  })
-);
-
 // Express static paths
 app.use("/img", express.static("./assets/img"));
 app.use("/font", express.static("./assets/font"));
@@ -83,9 +72,20 @@ app.use("/signup", express.static("./views/signup"));
 app.use("/login", express.static("./views/login"));
 app.use("/plantepedia", express.static("./views/plantepedia"));
 app.use("/plantepediaSummary", express.static("./views/plantepedia/summary"));
+app.use("/plantepediaDetail", express.static("./views/plantepedia/plantDetail"));
 app.use("/garden", express.static("./views/garden"));
 app.use("/profile", express.static("./views/profile"));
 app.use("/explore", express.static("./views/explore"));
+
+//session
+app.use(
+  session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true,
+  })
+);
 
 // TODO: create middleware - makes sure user is logged in otherwise gets redirected to login page (implement for every route)
 
@@ -105,12 +105,13 @@ function sessionValidation(req, res, next) {
 
 // LANDING PAGE
 app.get("/", (req, res) => {
-  if (req.session.authenticated) {
+  if(req.session.authenticated){
     res.render("home/home");
   } else {
     res.render("landing/landing");
   }
 });
+
 
 // SIGNUP PAGE
 app.get("/signup", async (req, res) => {
@@ -131,7 +132,7 @@ app.post("/signup/submitUser", async (req, res) => {
     password: Joi.string().max(20).required(),
   });
 
-  const validationResult = schema.validate({ name, username, password, email });
+  const validationResult = schema.validate({ name, username, password , email});
   if (validationResult.error != null) {
     console.log(validationResult.error);
     res.redirect("/signup");
@@ -176,21 +177,19 @@ app.post("/login/logging", async (req, res) => {
     password: Joi.string().max(20).required(),
   });
 
-  const validationResult = schema.validate({ email, password });
+const validationResult = schema.validate({ email, password });
 
-  if (validationResult.error != null) {
-    console.log(validationResult.error);
-    res.redirect("/login");
-    return;
-  }
-  const result = await userCollection
-    .find({ email: email })
-    .project({ email: 1, password: 1, _id: 1, username: 1 })
-    .toArray();
+	if (validationResult.error != null) {
+	   console.log(validationResult.error);
+	   res.redirect("/login");
+	   return;
+	}
+  const result = await userCollection.find({email: email}).project({email: 1, password: 1, _id: 1, username: 1}).toArray();
+
 
   if (result.length != 1) {
     res.redirect("/login");
-    console.log("no email");
+    console.log("no email")
     return;
   }
 
@@ -274,7 +273,7 @@ app.get("/profile", async (req, res) => {
 // PLANTEPEDIA SUMMARY PAGE
 app.get("/plantepediaSummary", async (req, res) => {
   // TODO Need to Image column later
-  const result = await plantSummaryCollection
+  const result = await plantCollection
     .find()
     .project({
       plant_name: 1,
@@ -285,14 +284,28 @@ app.get("/plantepediaSummary", async (req, res) => {
       nutrition: 1,
     })
     .toArray();
-  res.render("plantepedia/summary/plantepedia", { summaries: result });
+  console.log(result);
+  res.render("plantepedia/summary/plantepediaAllPlants", { summaries: result });
 });
 
-// TODO Need to add plantepediaDetail page
 // PLANTEPEDIA Plant's Detail PAGE
-// app.get("/plantepediaDetail", async (req, res) => {
-//   res.render("")
-// });
+app.get("/plantepediaDetail/:plant", async (req, res) => {
+    var plantName = req.params.plant;
+    // TODO Need to Image column later
+    const result = await plantCollection.find({plant_name: plantName}).project({
+        about: 1, 
+        prepare: 1, 
+        how: 1, 
+        tips: 1
+      })
+      .toArray();
+  console.log(result);
+  res.render("plantepedia/plantDetail/plantInfo", {
+    plantAbout: result[0].about, 
+    plantPrepare: result[0].prepare,
+    plantHow: result[0].how, 
+    plantTips: result[0].tips});
+});
 
 // COMUNITY PAGE
 app.get("/community", async (req, res) => {
