17cb80ff422f79ca85e2623ed19917a9fe75a15f
EvanVink <evink2@my.bcit.ca>
2024-05-23 21:07:44 -0700

    deleted modal function in community.js and connecting with tommy's code

--- views/community/community.js
+++ views/community/community.js
@@ -1,40 +1,5 @@
 
 
-function comment(userArray, commentsArray) {
-    console.log("hello")
-
-    const setup = async () => {
-        $('.modal-body').empty();
-        let user = JSON.parse(userArray)
-        let comments = JSON.parse(commentsArray)
-        console.log(JSON.parse(userArray))
-        for (let i = 0; i < user.length; i++){
-
-        
-        $('#viewC').append(`
-        
-        
-        <h3>${user[i]} 
-        <br>
-        <br>
-        ${comments[i]}</h3>
-        <br>
-        <h3>Comments!</h3>
-        <br>
-        `)
-
-        }
-
-    }
-
-
-    $(document).ready(setup);
-
-
-}
-
-
-
 function alert () {
 
     Swal.fire({
@@ -43,7 +8,7 @@ function alert () {
     icon: "success",
     showConfirmButton: false
     }
-  ) 
+  )
     
 
 }
